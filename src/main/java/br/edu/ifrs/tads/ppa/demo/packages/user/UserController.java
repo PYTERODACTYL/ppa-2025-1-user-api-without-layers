83b440618b2516791d1cbf382c272db9a54c160e
PYTERODACTYL <pyter512@live.com>
2025-04-28 18:17:32 -0300

    remove imports desnecessarios

--- src/main/java/br/edu/ifrs/tads/ppa/demo/packages/user/UserController.java
+++ src/main/java/br/edu/ifrs/tads/ppa/demo/packages/user/UserController.java
@@ -1,14 +1,10 @@
 package br.edu.ifrs.tads.ppa.demo.packages.user;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifrs.tads.ppa.demo.packages.profile.Profile;
-import br.edu.ifrs.tads.ppa.demo.packages.profile.Profile.AccountType;
-import br.edu.ifrs.tads.ppa.demo.packages.role.Role;
-import br.edu.ifrs.tads.ppa.demo.packages.role.RoleRepository;
 import br.edu.ifrs.tads.ppa.demo.packages.user.newUser.NewUser;
-import br.edu.ifrs.tads.ppa.demo.packages.user.UserService;
 
 
 
